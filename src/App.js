fac3b78b211839e01df23fd5a2f012f7ff1a1018
AlexCrosby <54353909+AlexCrosby@users.noreply.github.com>
2021-01-05 14:58:32 +0000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <head>
+                <title>Car Stream</title>
+            </head>
+            <body>
+            <h1>Car Stream</h1>
+            <img src="http://127.0.0.1:5000/video_feed"/>
+            </body>
+        </div>
+);
 }
 
 export default App;
